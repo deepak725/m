666cb765f296a911f181e940fbc3865a3743e111
deepak725 <64675537+deepak725@users.noreply.github.com>
2021-02-23 19:11:30 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/tewfmifwvcc823g/export.pkl?dl=0'
+export_file_url = 'https://drive.google.com/file/d/1YKVoM8q5s6zzqruzFZBDWLShM4rD88MY/view?usp=sharing'
 export_file_name = 'export.pkl'
 
 classes = ['Happy', 'sad']
